80da437c386359994049067efadd72518b9163bc
sevvalbrt <sevval.barut@hotmail.com>
2021-02-03 16:35:54 +0300

    first commit

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            foreach (var cars in carManager.GetAll())
+            {
+                Console.WriteLine("Description:"+ cars.Description+"\t"+"ModelYear:"+ cars.ModelYear);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,53 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _car;
+
+        public InMemoryCarDal()
+        {
+            _car = new List<Car>
+            {
+                new Car{ Id=1, BrandId=1, ColorId=1, DailyPrice=500, ModelYear=1980, Description="Car 1"},
+                new Car{ Id=2, BrandId=1, ColorId=2, DailyPrice=800, ModelYear=1990, Description="Car 2"},
+                new Car{ Id=3, BrandId=1, ColorId=3, DailyPrice=1000, ModelYear=1978, Description="Car 3"}
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _car.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _car.SingleOrDefault(c => c.Id == car.Id);
+            _car.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _car;
+        }
+
+        public List<Car> GetById(int carId)
+        {
+            return _car.Where(c => c.Id == carId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _car.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.Description = car.Description;
+            carToUpdate.DailyPrice = car.DailyPrice;
+        }
+    }
+}
