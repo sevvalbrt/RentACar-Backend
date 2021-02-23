866b2848db565b5291d2a4fe1725444cab3a82f7
sevvalbrt <sevval.barut@hotmail.com>
2021-02-23 21:47:45 +0300

    Added Validation Aspect, AOP

--- Business/Concrete/ColorManager.cs
+++ Business/Concrete/ColorManager.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
@@ -20,10 +21,9 @@ namespace Business.Concrete
             _colorDal = colorDal;
         }
 
+        [ValidationAspect(typeof(ColorValidator))]
         public IResult Add(Color color)
         {
-            ValidationTool.Validate(new ColorValidator(), color);
-
             _colorDal.Add(color);
             return new SuccessResult(Messages.ColorAdded);
         }
