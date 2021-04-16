49637442f00e43dc0473a4d9abfcad43721e6b0c
sevvalbrt <sevval.barut@hotmail.com>
2021-04-16 14:55:53 +0300

    Update Car Images

--- WebAPI/Controllers/CarImagesController.cs
+++ WebAPI/Controllers/CarImagesController.cs
@@ -55,7 +55,7 @@ namespace WebAPI.Controllers
         }
 
         [HttpPost("add")]
-        public IActionResult Add([FromForm(Name = ("Images"))] IFormFile file, [FromForm] CarImage carImage)
+        public IActionResult AddAsync([FromForm(Name = ("Image"))] IFormFile file, [FromForm] CarImage carImage)
         {
             var result = _carImageService.Add(file, carImage);
 
